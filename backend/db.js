9562de300b04ab55bc2fd39d3a9f0b39ba3d7a98
Nice Rose C J <127591159+nicerose7@users.noreply.github.com>
2025-07-04 16:16:54 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 //1.mongode insatll => npm i mongoose
 var mongoose = require("mongoose");
 //mongoose.connect("url").then(()=>{}).catch(()=>{})
-mongoose.connect("mongodb+srv://nicerose7732:nicerose@cluster0.wgjtytg.mongodb.net/ict?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("") //replace with databse url 
 .then(()=>{
     console.log("Db connected")
 })
